eb3b47b3ef4cbb96f442f26bfe968b2d2f412e67
Greyson Wright <greysonmwright@gmail.com>
2016-09-20 15:15:29 -0500

    - tabs instead of spaces

--- GPXTractor/GPXTractor/ImageExif.cs
+++ GPXTractor/GPXTractor/ImageExif.cs
@@ -6,76 +6,76 @@ using System.Text;
 using System.Xml;
 
 namespace GPXTractor {
-    class ImageExif {
-        private DateTime imageDateTime;
-        public string name;
-        public string path;
-        public string lattitude;
-        public string longitude;
-        public string dateTimeTaken;
-        public string model;
-        public string fieldOfView;
-        public string heading;
-        public bool gpsDidTimeOut;
+	class ImageExif {
+		private DateTime imageDateTime;
+		public string name;
+		public string path;
+		public string lattitude;
+		public string longitude;
+		public string dateTimeTaken;
+		public string model;
+		public string fieldOfView;
+		public string heading;
+		public bool gpsDidTimeOut;
 
-        public ImageExif(string imagePath, DateTime? offsetDateTime, XmlNodeList gpxData) {
-            FileStream imageStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
-            Image image = Image.FromStream(imageStream, false, false);
-            PropertyItem dateProperty = image.GetPropertyItem(36867);
-            PropertyItem cameraModel = image.GetPropertyItem(0x0110);
-            imageStream.Close();
+		public ImageExif(string imagePath, DateTime? offsetDateTime, XmlNodeList gpxData) {
+			FileStream imageStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+			Image image = Image.FromStream(imageStream, false, false);
+			PropertyItem dateProperty = image.GetPropertyItem(36867);
+			PropertyItem cameraModel = image.GetPropertyItem(0x0110);
+			imageStream.Close();
 
-            string takenTime = Encoding.UTF8.GetString(dateProperty.Value);
-            takenTime = takenTime.Remove(takenTime.Length - 1);
-            imageDateTime = DateTime.ParseExact(takenTime, "yyyy:MM:dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+			string takenTime = Encoding.UTF8.GetString(dateProperty.Value);
+			takenTime = takenTime.Remove(takenTime.Length - 1);
+			imageDateTime = DateTime.ParseExact(takenTime, "yyyy:MM:dd HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
 
-            name = Path.GetFileName(imagePath);
-            path = imagePath;
-            dateTimeTaken = correctImageDateTime(imageDateTime, offsetDateTime);
-            model = Encoding.UTF8.GetString(cameraModel.Value);
-            XmlNode gpxNode = getImageDetails(dateTimeTaken, gpxData);
-            lattitude = gpxNode.Attributes.Item(0).Value;
-            longitude = gpxNode.Attributes.Item(1).Value;
-            heading = gpxNode.ChildNodes.Item(3).ChildNodes.Item(2).InnerText;
-            fieldOfView = model.ToLower().Contains("iphone") ? "63.7" : "67.1";
-        }
+			name = Path.GetFileName(imagePath);
+			path = imagePath;
+			dateTimeTaken = correctImageDateTime(imageDateTime, offsetDateTime);
+			model = Encoding.UTF8.GetString(cameraModel.Value);
+			XmlNode gpxNode = getImageDetails(dateTimeTaken, gpxData);
+			lattitude = gpxNode.Attributes.Item(0).Value;
+			longitude = gpxNode.Attributes.Item(1).Value;
+			heading = gpxNode.ChildNodes.Item(3).ChildNodes.Item(2).InnerText;
+			fieldOfView = model.ToLower().Contains("iphone") ? "63.7" : "67.1";
+		}
 
-        public void writeToFile(string photographer, StreamWriter streamWriter) {
-            streamWriter.Write(name + "," + path + "," + lattitude + "," + longitude + "," + model + "," + heading + "," + fieldOfView + "," + photographer);
-            if(gpsDidTimeOut) {
-                streamWriter.Write(",Potential GPS Error");
-            }
-            streamWriter.Write("\r\n");
-        }
+		public void writeToFile(string photographer, StreamWriter streamWriter) {
+			streamWriter.Write(name + "," + path + "," + lattitude + "," + longitude + "," + model + "," + heading + "," + fieldOfView + "," + photographer);
+			if(gpsDidTimeOut) {
+				streamWriter.Write(",Potential GPS Error");
+			}
+			streamWriter.Write("\r\n");
+		}
 
-        private string correctImageDateTime(DateTime timeTaken, DateTime? offsetDateTime) {
-            DateTime timeDifference = timeTaken.Subtract(offsetDateTime.Value.TimeOfDay);
-            return timeDifference.Subtract(timeDifference.TimeOfDay).ToString();
-        }
+		private string correctImageDateTime(DateTime timeTaken, DateTime? offsetDateTime) {
+			DateTime timeDifference = timeTaken.Subtract(offsetDateTime.Value.TimeOfDay);
+			return timeDifference.Subtract(timeDifference.TimeOfDay).ToString();
+		}
 
-        private XmlNode getImageDetails(string dateString, XmlNodeList gpxData) {
-            int index = getGPXPosition(imageDateTime, gpxData);
-            return gpxData[index];
-        }
+		private XmlNode getImageDetails(string dateString, XmlNodeList gpxData) {
+			int index = getGPXPosition(imageDateTime, gpxData);
+			return gpxData[index];
+		}
 
-        private int getGPXPosition(DateTime currentDateTime, XmlNodeList gpxData) {
-            long minDifference = long.MaxValue;
-            int index = 0;
+		private int getGPXPosition(DateTime currentDateTime, XmlNodeList gpxData) {
+			long minDifference = long.MaxValue;
+			int index = 0;
 
-            for(int i = 0; i < gpxData.Count; i++) {
-                DateTime gpxDate = Convert.ToDateTime(gpxData.Item(i).ChildNodes.Item(1).InnerText);
-                long difference = Math.Abs(gpxDate.TimeOfDay.Ticks - currentDateTime.TimeOfDay.Ticks);
-                if(minDifference > difference) {
-                    minDifference = difference;
-                    index = i;
-                }
-            }
+			for(int i = 0; i < gpxData.Count; i++) {
+				DateTime gpxDate = Convert.ToDateTime(gpxData.Item(i).ChildNodes.Item(1).InnerText);
+				long difference = Math.Abs(gpxDate.TimeOfDay.Ticks - currentDateTime.TimeOfDay.Ticks);
+				if(minDifference > difference) {
+					minDifference = difference;
+					index = i;
+				}
+			}
 
-            if(minDifference > TimeSpan.FromMinutes(1).Ticks) {
-                gpsDidTimeOut = true;
-            }
+			if(minDifference > TimeSpan.FromMinutes(1).Ticks) {
+				gpsDidTimeOut = true;
+			}
 
-            return index;
-        }
-    }
+			return index;
+		}
+	}
 }
--- GPXTractor/GPXTractor/MainWindow.xaml.cs
+++ GPXTractor/GPXTractor/MainWindow.xaml.cs
@@ -6,119 +6,119 @@ using System.Xml;
 using Microsoft.Win32;
 
 namespace GPXTractor {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow :Window {
-        string[] imagePaths;
-        List<string> imageDates = new List<string>();
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow :Window {
+		string[] imagePaths;
+		List<string> imageDates = new List<string>();
 
-        public MainWindow() {
-            InitializeComponent();
-        }
+		public MainWindow() {
+			InitializeComponent();
+		}
 
-        string openFile(string fileType) {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Filter = fileType;
-            fileDialog.ShowDialog();
+		string openFile(string fileType) {
+			OpenFileDialog fileDialog = new OpenFileDialog();
+			fileDialog.Filter = fileType;
+			fileDialog.ShowDialog();
 
-            if(!string.IsNullOrWhiteSpace(fileDialog.FileName)) {
-                return fileDialog.FileName;
-            }
+			if(!string.IsNullOrWhiteSpace(fileDialog.FileName)) {
+				return fileDialog.FileName;
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        string[] openFolder(TextBox textBox) {
-            System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog();
-            folderDialog.ShowDialog();
+		string[] openFolder(TextBox textBox) {
+			System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+			folderDialog.ShowDialog();
 
-            if(!string.IsNullOrWhiteSpace(folderDialog.SelectedPath)) {
-                textBox.Text = folderDialog.SelectedPath;
-                return Directory.GetFiles(folderDialog.SelectedPath);
-            }
+			if(!string.IsNullOrWhiteSpace(folderDialog.SelectedPath)) {
+				textBox.Text = folderDialog.SelectedPath;
+				return Directory.GetFiles(folderDialog.SelectedPath);
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        string saveFile(string fileType) {
-            SaveFileDialog saveDialog = new SaveFileDialog();
-            saveDialog.Filter = fileType;
-            saveDialog.ShowDialog();
+		string saveFile(string fileType) {
+			SaveFileDialog saveDialog = new SaveFileDialog();
+			saveDialog.Filter = fileType;
+			saveDialog.ShowDialog();
 
-            if(!string.IsNullOrEmpty(saveDialog.FileName)) {
-                return saveDialog.FileName;
-            }
+			if(!string.IsNullOrEmpty(saveDialog.FileName)) {
+				return saveDialog.FileName;
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        private void writeImageExifs(ImageExif[] imageExifs, string path) {
-            StreamWriter streamWriter = new StreamWriter(path);
-            streamWriter.WriteLine("Image Name,File Path,Lattitude,Longitude,Model,Heading,Field Of View,Photographer");
-            foreach(ImageExif imageExif in imageExifs) {
-                imageExif.writeToFile(photographerTextBox.Text, streamWriter);
-            }
-            streamWriter.Close();
-        }
+		private void writeImageExifs(ImageExif[] imageExifs, string path) {
+			StreamWriter streamWriter = new StreamWriter(path);
+			streamWriter.WriteLine("Image Name,File Path,Lattitude,Longitude,Model,Heading,Field Of View,Photographer");
+			foreach(ImageExif imageExif in imageExifs) {
+				imageExif.writeToFile(photographerTextBox.Text, streamWriter);
+			}
+			streamWriter.Close();
+		}
 
-        private void gpxButton_Click(object sender, RoutedEventArgs e) {
-            string imageDirectory = openFile("GPX Files|*.gpx");
-            gpxTextBox.Text = imageDirectory;
-            imageDirectoryTextBox.Text = Path.GetDirectoryName(imageDirectory);
-        }
+		private void gpxButton_Click(object sender, RoutedEventArgs e) {
+			string imageDirectory = openFile("GPX Files|*.gpx");
+			gpxTextBox.Text = imageDirectory;
+			imageDirectoryTextBox.Text = Path.GetDirectoryName(imageDirectory);
+		}
 
-        private void imageDirectoryButton_Click(object sender, RoutedEventArgs e) {
-            imagePaths = openFolder(imageDirectoryTextBox);
-        }
+		private void imageDirectoryButton_Click(object sender, RoutedEventArgs e) {
+			imagePaths = openFolder(imageDirectoryTextBox);
+		}
 
-        private void outputDirectoryButton_Click(object sender, RoutedEventArgs e) {
-            outputDirectoryTextBox.Text = saveFile("CSV Files|*.csv");
-        }
+		private void outputDirectoryButton_Click(object sender, RoutedEventArgs e) {
+			outputDirectoryTextBox.Text = saveFile("CSV Files|*.csv");
+		}
 
-        private void generateButton_Click(object sender, RoutedEventArgs e) {
-            List<ImageExif> imageExifs = new List<ImageExif>();
+		private void generateButton_Click(object sender, RoutedEventArgs e) {
+			List<ImageExif> imageExifs = new List<ImageExif>();
 
-            if(gpxTextBox.Text != "" && imageDirectoryTextBox.Text != "" && outputDirectoryTextBox.Text != "" && photographerTextBox.Text != "") {
-                XmlDocument gpxfile = new XmlDocument();
-                gpxfile.Load(gpxTextBox.Text);
-                XmlNodeList dataPoints = gpxfile.GetElementsByTagName("trkpt");
+			if(gpxTextBox.Text != "" && imageDirectoryTextBox.Text != "" && outputDirectoryTextBox.Text != "" && photographerTextBox.Text != "") {
+				XmlDocument gpxfile = new XmlDocument();
+				gpxfile.Load(gpxTextBox.Text);
+				XmlNodeList dataPoints = gpxfile.GetElementsByTagName("trkpt");
 
-                if(imagePaths == null) {
-                    imagePaths = Directory.GetFiles(imageDirectoryTextBox.Text);
-                }
+				if(imagePaths == null) {
+					imagePaths = Directory.GetFiles(imageDirectoryTextBox.Text);
+				}
 
-                foreach(var imagePath in imagePaths) {
-                    if(imagePath.Contains(".jpg") || imagePath.Contains(".JPG") || imagePath.Contains(".png")) {
-                        ImageExif imageExif = new ImageExif(imagePath, dateTimePicker.Value, dataPoints);
-                        imageExifs.Add(imageExif);
-                    }
-                }
+				foreach(var imagePath in imagePaths) {
+					if(imagePath.Contains(".jpg") || imagePath.Contains(".JPG") || imagePath.Contains(".png")) {
+						ImageExif imageExif = new ImageExif(imagePath, dateTimePicker.Value, dataPoints);
+						imageExifs.Add(imageExif);
+					}
+				}
 
-                writeImageExifs(imageExifs.ToArray(), outputDirectoryTextBox.Text);
-                MessageBox.Show("Task complete.");
-            } else {
-                MessageBox.Show("Please make sure each field has been completed.");
-            }
-        }
-        private void viewImageButton_Click(object sender, RoutedEventArgs e) {
-            if(imageDirectoryTextBox.Text != "") {
-                string firstImagePath = null;
+				writeImageExifs(imageExifs.ToArray(), outputDirectoryTextBox.Text);
+				MessageBox.Show("Task complete.");
+			} else {
+				MessageBox.Show("Please make sure each field has been completed.");
+			}
+		}
+		private void viewImageButton_Click(object sender, RoutedEventArgs e) {
+			if(imageDirectoryTextBox.Text != "") {
+				string firstImagePath = null;
 
-                if(imagePaths == null) {
-                    imagePaths = Directory.GetFiles(imageDirectoryTextBox.Text);
-                }
-                foreach(var imagePath in imagePaths) {
-                    if(imagePath.Contains(".jpg") || imagePath.Contains(".JPG") || imagePath.Contains(".png")) {
-                        firstImagePath = imagePath;
-                    }
-                }
-                if(firstImagePath != null) {
-                    System.Diagnostics.Process.Start(firstImagePath);
-                    return;
-                }
-            }
-            MessageBox.Show("No image found in the selected directory.");
-        }
-    }
+				if(imagePaths == null) {
+					imagePaths = Directory.GetFiles(imageDirectoryTextBox.Text);
+				}
+				foreach(var imagePath in imagePaths) {
+					if(imagePath.Contains(".jpg") || imagePath.Contains(".JPG") || imagePath.Contains(".png")) {
+						firstImagePath = imagePath;
+					}
+				}
+				if(firstImagePath != null) {
+					System.Diagnostics.Process.Start(firstImagePath);
+					return;
+				}
+			}
+			MessageBox.Show("No image found in the selected directory.");
+		}
+	}
 }
